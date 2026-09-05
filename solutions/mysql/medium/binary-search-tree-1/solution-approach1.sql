ee804dd79ee1a4471b702cace982ccf6abfe757b
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:28:11 +0530

    ✅ Binary Tree Nodes [mysql] — approach1

---
+++ solutions/mysql/medium/binary-search-tree-1/solution-approach1.sql
@@ -0,0 +1,19 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+-- Problem     Binary Tree Nodes
+-- Difficulty  Medium
+-- Subdomain   Advanced Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:28 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT N,
+CASE 
+    WHEN P IS NULL THEN 'Root'
+    WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+    ELSE 'Leaf'
+END
+FROM BST
+ORDER BY N;
