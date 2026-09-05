66d7f211a7c01d3da2d954e88a7689e3a8d8c9d0
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:21:58 +0530

    ✅ Select By ID [mysql] — approach1

---
+++ solutions/mysql/easy/select-by-id/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- Problem     Select By ID
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:21 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT*FROM CITY WHERE ID=1661;
