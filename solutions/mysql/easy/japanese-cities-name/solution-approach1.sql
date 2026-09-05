a3ac21cad3b32d67752384797f5b5493fa0652c5
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:25:11 +0530

    ✅ Japanese Cities' Names [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-name/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-name/problem?isFullScreen=true
+-- Problem     Japanese Cities' Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:25 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
