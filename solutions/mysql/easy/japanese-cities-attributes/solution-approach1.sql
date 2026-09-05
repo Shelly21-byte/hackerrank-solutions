88b177959d0c170bccac99b1d35c883b0efa1b75
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:23:14 +0530

    ✅ Japanese Cities' Attributes [mysql] — approach1

---
+++ solutions/mysql/easy/japanese-cities-attributes/solution-approach1.sql
@@ -0,0 +1,12 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/japanese-cities-attributes/problem?isFullScreen=true
+-- Problem     Japanese Cities' Attributes
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:23 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT * FROM CITY WHERE COUNTRYCODE = 'JPN';
