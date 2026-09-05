7ea4062de24ee5a0af041a739c4610656d365200
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:19:27 +0530

    ✅ Weather Observation Station 5 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-5/solution-approach1.sql
@@ -0,0 +1,16 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=true
+-- Problem     Weather Observation Station 5
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:19 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT CITY, LENGTH(CITY) FROM STATION 
+ORDER BY LENGTH(CITY) ASC, CITY ASC LIMIT 1;
+
+SELECT CITY, LENGTH(CITY) FROM STATION 
+ORDER BY LENGTH(CITY) DESC, CITY ASC LIMIT 1;
