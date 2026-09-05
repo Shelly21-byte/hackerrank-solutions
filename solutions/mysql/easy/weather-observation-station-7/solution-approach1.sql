8f212567323f1c04131eb5e542c64b0240ebd5ec
Shelly Sharma <shelly200421@gmail.com>
2026-09-05 17:06:36 +0530

    ✅ Weather Observation Station 7 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-7/solution-approach1.sql
@@ -0,0 +1,17 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-7/problem?isFullScreen=true
+-- Problem     Weather Observation Station 7
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-05, 05:06 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT DISTINCT CITY FROM STATION 
+WHERE CITY LIKE '%a' 
+OR CITY LIKE '%e' 
+OR CITY LIKE '%i' 
+OR CITY LIKE '%o' 
+OR CITY LIKE '%u';
