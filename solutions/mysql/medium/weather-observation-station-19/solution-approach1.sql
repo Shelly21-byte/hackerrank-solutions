4ab188c82619fb652bbb6dd018a213341fa7c4c8
Shelly Sharma <shelly200421@gmail.com>
2026-09-06 18:27:20 +0530

    ✅ Weather Observation Station 19 [mysql] — approach1

---
+++ solutions/mysql/medium/weather-observation-station-19/solution-approach1.sql
@@ -0,0 +1,18 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-19/problem?isFullScreen=true
+-- Problem     Weather Observation Station 19
+-- Difficulty  Medium
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-06, 06:27 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT
+  ROUND(
+    SQRT(
+      POWER(MAX(LAT_N) - MIN(LAT_N), 2) + 
+      POWER(MAX(LONG_W) - MIN(LONG_W), 2)
+    ), 4)
+FROM STATION;
