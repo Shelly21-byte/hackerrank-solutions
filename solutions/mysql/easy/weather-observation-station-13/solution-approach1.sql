2b65387090105498774c9f71b00e2adeeac92970
Shelly Sharma <shelly200421@gmail.com>
2026-09-10 20:25:55 +0530

    ✅ Weather Observation Station 13 [mysql] — approach1

---
+++ solutions/mysql/easy/weather-observation-station-13/solution-approach1.sql
@@ -0,0 +1,15 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/weather-observation-station-13/problem?isFullScreen=true
+-- Problem     Weather Observation Station 13
+-- Difficulty  Easy
+-- Subdomain   Aggregation
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-10, 08:25 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT TRUNCATE(SUM(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 
+AND LAT_N < 137.2345;
