1e8c0e4539a1522c1304e4d3d1675c20c3abf111
Shelly Sharma <shelly200421@gmail.com>
2026-09-15 23:15:49 +0530

    ✅ Employee Names [mysql] — approach1

---
+++ solutions/mysql/easy/name-of-employees/solution-approach1.sql
@@ -0,0 +1,14 @@
+-- ──────────────────────────────────────────────────
+-- Link        https://www.hackerrank.com/challenges/name-of-employees/problem?isFullScreen=true
+-- Problem     Employee Names
+-- Difficulty  Easy
+-- Subdomain   Basic Select
+-- Platform    HackerRank
+-- Language    mysql
+-- Status      Accepted
+-- Submitted   2026-09-15, 11:15 p.m.
+-- ──────────────────────────────────────────────────
+
+SELECT name
+FROM employee
+ORDEr BY  name ASC;
